c01c7ff1f256fc2fd9f44739b0fc5c81a0ae4c59
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-26 21:03:29 +0330

    add MovieCard component

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const root = ReactDOM.createRoot(container);
 
 const getUrl = query => `https://api.themoviedb.org/3/search/movie?api_key=c560a5cf8d65ebc0f13dc245d96be4f3&language=en-US&query=${query}&page=1&include_adult=false`;
 
+function MovieCard({ movie }) {
+    return (
+        <div className='card'>
+            <img
+                className='card--image'
+                src={movie.poster_path ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}` : 'https://via.placeholder.com/185x278'}
+                alt={movie.title}
+            />
+            <div className='card--content'>
+                <h3 className='card--title'>{movie.title}</h3>
+                <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                <p><small>RATING: {movie.vote_average}</small></p>
+                <p className='card--desc'>{movie.overview}</p>
+            </div>
+        </div>
+    );
+}
+
 function SearchMovies() {
     const [query, setQuery] = React.useState('');
     const [loading, setLoading] = React.useState(false);
@@ -47,21 +65,7 @@ function SearchMovies() {
                 {error && <p className='error'>{error}</p>}
             </form>
             <div className='card-list'>
-                {movies.map(movie => (
-                    <div className='card' key={movie.id}>
-                        <img
-                            className='card--image'
-                            src={movie.poster_path ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}` : 'https://via.placeholder.com/185x278'}
-                            alt={movie.title}
-                        />
-                        <div className='card--content'>
-                            <h3 className='card--title'>{movie.title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className='card--desc'>{movie.overview}</p>
-                        </div>
-                    </div>
-                ))}
+                {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
             </div>
         </React.Fragment>
     );
