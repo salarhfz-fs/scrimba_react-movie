861b123c53e599993bb8ca4d87edd08980efe263
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-26 19:43:31 +0330

    add SearchMovies component

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+function SearchMovies() {
+    return (
+        <form className='form'>
+            <label htmlFor='query' className='label'>
+                Movie name
+            </label>
+            <input className='input' type='text' id='query' name='query' placeholder='i.e. Leon the Professional' />
+            <button className='button' type='submit'>Search</button>
+        </form>
+    );
+}
+
 function App() {
     return (
         <div className='container'>
             <h1 className='title'>React Movie Search</h1>
+            <SearchMovies />
         </div>
     );
 }
